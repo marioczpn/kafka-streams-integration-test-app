458f7ee6ab1dc2ff7a0446a7e9ef960f784fd9ed
Mario Ponciano <marioczpn@gmail.com>
2020-11-28 14:40:48 -0300

    Fixing slf4j dependencies

--- src/main/java/com/github/marioczpn/kafka/streams/KafkaStreamsIntegrationTestMainApp.java
+++ src/main/java/com/github/marioczpn/kafka/streams/KafkaStreamsIntegrationTestMainApp.java
@@ -1,6 +1,7 @@
 package com.github.marioczpn.kafka.streams;
 
 import com.github.marioczpn.kafka.streams.controller.KafkaManagerController;
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -14,6 +15,12 @@ public class KafkaStreamsIntegrationTestMainApp {
 
         KafkaManagerController kafkaManagerController = new KafkaManagerController();
         String action = System.getenv("KAFKA_ACTION_ENVVAR");
+        if(StringUtils.isBlank(action)) {
+            final String errorMsg = "Please set the KAFKA_ACTION_ENVVAR (environment variable), it is missed.";
+            logger.error(errorMsg);
+            throw new IllegalArgumentException(errorMsg);
+        }
+
         switch (action) {
             case "PRODUCER":
                 kafkaManagerController.startProducer();
@@ -22,7 +29,7 @@ public class KafkaStreamsIntegrationTestMainApp {
                 kafkaManagerController.startConsumer();
                 break;
             default:
-                logger.info("Sorry, KAFKA_ACTION doesn't exists.");
+                logger.info("Sorry, This KAFKA_ACTION doesn't exists. Check the Environment variable (KAFKA_ACTION_ENVVAR). ");
                 break;
         }
 
